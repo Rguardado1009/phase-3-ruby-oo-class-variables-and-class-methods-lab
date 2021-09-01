b47d14f261726a21bff9878f94087042d7ea78dc
Rguardado1009 <rguardado1009@gmail.com>
2021-09-01 12:31:07 -0400

    commit tree 80f04fe89ae71ceaac6b8a2ba85665f52c49be8a

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+ class song
+    attr_accessor :name, :artist, :genre
+    
+    def name
+      @name = name
+    end
+    def artist
+      @artist = artist
+    end
+    def genre
+        @genre = genre
+    end
+
+end
